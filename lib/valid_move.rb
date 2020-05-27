d12125ce9cfe0cde5da314cde6435454b1985432
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-27 23:40:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if board[index] == " " || board[index] == ''
     true
-  elsif board[index] == "X" || board[index] == "O" || board[index] < 0 && board[index] > 9
+  elsif board[index] == "X" || board[index] == "O" || index < 0 || index > 9
     false
   end
 
